d6b4c0a7e7d34741c70b8b500128fcd1a8334eb6
Administrator <Administrator@192.168.0.105>
2020-08-01 11:15:20 +0800

    他人修改

---
+++ three/src/com/yc/c729/Cookie.java
@@ -0,0 +1,43 @@
+package com.yc.c729;
+
+public class Cookie {
+
+	
+	private String name;
+	private String value;
+	private int maxAge=-1;
+	public Cookie(String name,String value){
+		this.name=name;
+		this.value=value;
+	}
+	
+	public String toString(){
+		String s="Set-cookie: %s=%s;\n";
+		s=String.format(s, name,value);
+		
+		if(maxAge>-1){
+			s+="Max-Age="+maxAge+";";
+		}
+		s+="\n";
+		System.out.println("s="+s);
+		return s;
+	}
+	
+	public void setMaxAge(int maxAge){
+		this.maxAge=maxAge;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public int getMaxAge() {
+		System.out.println("==========");
+		return maxAge;
+	}
+	
+}
